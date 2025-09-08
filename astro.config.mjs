6689b8f45fd96aef1f89100da69dcabd4c735848
diegoaep07 <diegoaep07@proton.me>
2025-09-08 16:42:39 -0400

    Configurando el Deploy del sitio en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://smartpagesteam.github.io/',
   vite: {
     plugins: [tailwindcss()]
   },
